2b9f08deec2ccb498e0bb2190ec2904ea9297ba1
chenpeikai <18712733901@163.com>
2016-05-25 14:14:38 +0800

    停表小游戏

--- 2/clock.py
+++ 2/clock.py
@@ -27,19 +27,23 @@ def start_handler():
     timer.start()
 #check if you had stop it right
 def stop_handler():
-    global total_num , right_num , score_str
+    global total_num , right_num , score_str,is_stop
     if is_stop is True:
         return
     total_num = total_num + 1
     if tick%10 == 0 and tick%50 == 0:
         right_num = right_num + 1
     score_str = "%d/%d"%(right_num,total_num)
+    timer.stop()
+    is_stop = True
 def reset_handler():
-    global time_message,tick,is_stop
+    global time_message,tick,is_stop,total_num,right_num
     tick = 0
     timer.stop()
     time_message = '0:00.0'
     is_stop = True
+    total_num = 0
+    right_num = 0
 # define event handler for timer with 0.1 sec interval
 def timer_handler():
     global tick,time_message
@@ -48,10 +52,12 @@ def timer_handler():
 
 # define draw handler
 def draw_handler(canvas):
-    canvas.draw_text(time_message,(120,150),30,"White")
-    canvas.draw_text(score_str,(240,30),30,"Red")
+    global score_str
+    score_str = "%d/%d"%(right_num,total_num)
+    canvas.draw_text(time_message,(80,125),50,"White")
+    canvas.draw_text(score_str,(190,30),30,"Red")
 # create frame
-frame = simplegui.create_frame("clock" , 300 , 300)
+frame = simplegui.create_frame("clock" , 250 , 250)
 timer = simplegui.create_timer(100, timer_handler)
 # register event handlers
 frame.set_draw_handler(draw_handler)
